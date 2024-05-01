a83c842bb5f4f6cfcede48959c787d5d98f9dea9
annnie73 <aniaszymik02@gmail.com>
2024-05-01 10:16:33 +0200

    change file organisation

---
+++ draft.py
@@ -0,0 +1,23 @@
+# Wizualizacja -- na razie trochę nie mam pomysłu
+# x_values = []
+# y_values = []
+# colors = []
+
+# for component in candidates:
+#     for cell_id in component:
+#         x, y = G_all.nodes[cell_id]['pos']
+#         x_values.append(x)
+#         y_values.append(y)
+#         colors.append(IF1_cell_mapping[G_all.nodes[cell_id]['celltype']])
+
+# fig = go.Figure(data=go.Scattergl(
+#     x = x_values,
+#     y = y_values,
+#     mode = 'markers',
+#     marker = dict(
+#         color = colors,
+#         size = 10
+#     )
+# ))
+
+# fig.show()
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import networkx as nx
 import plotly.graph_objects as go
 from sklearn import neighbors
-from scipy.spatial import KDTree
 from helper import get_panel
-from sklearn.metrics import pairwise_distances
-
 
 patient = "1107"
 df = get_panel('IF1', patient)
-df.set_index('cell.ID')
+celltypes = df['celltype'].unique()
+
+def rgb_to_hex(rgb):
+    return '#' + '%02x%02x%02x' % rgb
+
+IF1_cell_mapping = {"other": rgb_to_hex((190, 190, 190)), 
+                    "CD15+Tumor": rgb_to_hex((73, 176, 248)),
+                    "CD15-Tumor": rgb_to_hex((138, 79, 45)),
+                    "Tcell": rgb_to_hex((235, 74, 148)),
+                    "Bcell": rgb_to_hex((204, 49, 31)),
+                    "BnTcell": rgb_to_hex((236, 95, 42)),
+                    "Neutrophil": rgb_to_hex((0, 40, 245)),
+                    "Macrophage": rgb_to_hex((97, 209, 62)),
+                    "DC": rgb_to_hex((49, 113, 30))}
+
+
+# graf tylko nad wybranym typem komórek (np. CK)
+# chcemy sklastrowac wektory udzialu procentowego poszczegolnych typów komórek
+
+# elementy na grafie TLS – szukamy sąsiadów w ogólnym grafie
+# potem mozemy zobaczyc ile u naszych pacjentów jest danego klastra, jakies ciekawe podsumowanie
 
 
 def graph_by_cell_type(df, cell_types=None):
@@ -17,126 +34,40 @@ def graph_by_cell_type(df, cell_types=None):
     if cell_types is not None:
         df = df[df['celltype'].isin(cell_types)]
 
-    adj_matrix = neighbors.radius_neighbors_graph(df[['nucleus.x', 'nucleus.y']], radius=30)  # macierz sąsiedztwa
+    adj_matrix = neighbors.radius_neighbors_graph(df[['nucleus.x', 'nucleus.y']], radius=30)  # macierz sąsiedztwa  
+    G = nx.from_scipy_sparse_array(adj_matrix)
 
-    G = nx.Graph()  # graf z danymi o komórkach
-    for i, (cell_id, cell_type, x, y) in enumerate(zip(df['cell.ID'], df['celltype'], df['nucleus.x'], df['nucleus.y'])):
-        G.add_node(cell_id)  # wierzchołki to cell_id
-        G.nodes[cell_id]['celltype'] = cell_type
-        G.nodes[cell_id]['pos'] = x, y
+    index_to_cell_id = df['cell.ID'].to_dict()
+    G = nx.relabel_nodes(G, index_to_cell_id)
 
-    # Krawędzie na podstawie macierzy sąsiedztwa
-    for i, row in enumerate(adj_matrix.toarray()):
-        for j, connected in enumerate(row):
-            if connected:
-                G.add_edge(df.iloc[i]['cell.ID'], df.iloc[j]['cell.ID'])
+    # Dodajemy dane o komórkach
+    for cell_id, cell_type, x, y in zip(df['cell.ID'], df['celltype'], df['nucleus.x'], df['nucleus.y']):
+        if cell_id in G.nodes:
+            G.nodes[cell_id]['celltype'] = cell_type
+            G.nodes[cell_id]['pos'] = x, y
 
     return G
 
 
-def connected_component_from_candidate(tree, TLS_candidate, distance=30):
-
-    def DFS(tree, start_point, visited=None, distance=distance):
-        if visited is None:
-            visited = set()
-
-        # Znajdź wszystkie punkty w określonej odległości od punktu startowego
-        indices = tree.query_ball_point(start_point, distance)
-
-        for index in indices:
-            if index not in visited:
-                visited.add(index)
-                DFS(tree, df.iloc[index][['nucleus.x', 'nucleus.y']], visited, distance)
-
-        return visited
-
-    # start_point = df[df['cell.ID'].isin(TLS_candidate.nodes)]['nucleus.x', 'nucleus.y'].iloc[0]
-    start_cell_id = next(iter(TLS_candidate))
-    start_coordinates = df.loc[start_cell_id, ['nucleus.x', 'nucleus.y']].values
-
-    # Teraz 'coordinates' to współrzędne punktu o danym 'cell_id' w KDTree
-    component = DFS(tree, start_coordinates)
-
-    print("Spójna składowa zawierająca punkt startowy to:", df.iloc[list(component)])
-
-    return component
-
-
-# KDTree wszystkich komórek
-# dist_matrix = pairwise_distances(df[['nucleus.x', 'nucleus.y']])
-tree = KDTree(df[['nucleus.x', 'nucleus.y']])
-
-# Spójne składowe, będące kandydatami na TLS
+G_all = graph_by_cell_type(df)
+all_connected_components = list(nx.connected_components(G_all))
 G_TLS_candidates = graph_by_cell_type(df, ['Tcell', 'Bcell', 'BnTcell'])
 TLS_components = nx.connected_components(G_TLS_candidates)
 
+candidates = set()
+
 for candidate in TLS_components:
-    if len(candidate) > 5:
-        connected_component_from_candidate(tree, candidate)
+    if len(candidate) < 5:
+        continue
+    start_cell_id = next(iter(candidate))
+    for component in all_connected_components:
+        if start_cell_id in component:
+            candidates.add(tuple(component))
+
+# kandydaci to teraz zbior krotek z id komórki
+# wsrod kazdego kandydata (o jakiejs sensownej dlugosci) chcemy go zwizualizowac 
+# i obliczyc procentowy udzial poszczegolnych typow komorek w tym klastrze
+# moze z kazdego zrobic graf i zwizualizowac je na jednym obrazku?        
 
 
 
-"""
-celltypes = list(set(G.nodes[i]['celltype'] for i in G.nodes))
-celltype_to_number = {celltype: number for number, celltype in enumerate(celltypes)}
-
-# Teraz możemy użyć tego słownika do kolorowania wierzchołków
-node_colors = [celltype_to_number[G.nodes[i]['celltype']] for i in G.nodes]
-
-
-edge_x = []
-edge_y = []
-for edge in G.edges():
-    x0, y0 = G.nodes[edge[0]]['pos']
-    x1, y1 = G.nodes[edge[1]]['pos']
-    edge_x.append(x0)
-    edge_x.append(x1)
-    edge_x.append(None)
-    edge_y.append(y0)
-    edge_y.append(y1)
-    edge_y.append(None)
-
-edge_trace = go.Scatter(
-    x=edge_x, y=edge_y,
-    line=dict(width=0.5, color='#888'),
-    hoverinfo='none',
-    mode='lines')
-
-node_x = []
-node_y = []
-for node in G.nodes():
-    x, y = G.nodes[node]['pos']
-    node_x.append(x)
-    node_y.append(y)
-
-node_trace = go.Scatter(
-    x=node_x, y=node_y,
-    mode='markers',
-    hoverinfo='text',
-    marker=dict(
-        showscale=True,
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=node_colors,
-        size=10,
-        colorbar=dict(
-            thickness=15,
-            title='Node Connections',
-            xanchor='left',
-            titleside='right'
-        ),
-        line_width=2))
-
-
-fig = go.Figure(data=[edge_trace, node_trace],
-             layout=go.Layout(
-                title='<br>Network graph made with Python',
-                titlefont_size=16,
-                showlegend=False,
-                hovermode='closest',
-                margin=dict(b=20,l=5,r=5,t=40),
-                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                )
-fig.show()
-"""
